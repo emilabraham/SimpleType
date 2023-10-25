8ffe2e7e958e236054394887bc359b6edff9dc41
Emil Abraham <emilabraham1414@gmail.com>
2023-10-24 18:07:40 -0700

    spawn with text

--- text.gd
+++ text.gd
@@ -1,9 +1,8 @@
-extends Node
-
+extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	text = "emilabraham"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

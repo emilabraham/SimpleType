8715f988e06da37a7039d2f3f0df8be4bbd7a43f
Emil Abraham <emilabraham1414@gmail.com>
2023-10-30 07:25:45 -0700

    keyboard input detection

--- text.gd
+++ text.gd
@@ -4,7 +4,19 @@ extends Label
 func _ready():
 	text = "emilabraham"
 
+func _input(event):
+	if event is InputEventKey:
+		contains_and_emit(event.as_text_key_label().to_lower())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+func contains_and_emit(key_label):
+	if text.substr(0, 1).contains(key_label):
+		update_text()
+
+func update_text():
+	text = text.substr(1, text.length() + 1)
+	if text.is_empty():
+		hide()

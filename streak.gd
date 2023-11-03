6a4a811008d1b802fd316d3f3f4f68707ab69386
Emil Abraham <emilabraham1414@gmail.com>
2023-11-03 07:11:44 -0700

    warning fixes

--- streak.gd
+++ streak.gd
@@ -8,7 +8,7 @@ func _ready():
 	text = str(streak)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func increment_streak():

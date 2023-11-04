31f1545d720f35b9f5b3f970f4784749d6d53539
Emil Abraham <emilabraham1414@gmail.com>
2023-11-03 18:20:35 -0700

    randomly spawn single text instance

--- main.gd
+++ main.gd
@@ -1,10 +1,11 @@
 extends Node
 
-
+var text = preload("res://text.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var text_instance_1 = text.instantiate()
+	text_instance_1.set_position(Vector2(randi_range(100, 1000), randi_range(100, 500)))
+	add_child(text_instance_1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):

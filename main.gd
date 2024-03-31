9b71385839fa1f794762d0db777505beb194525d
Emil Abraham <emilabraham1414@gmail.com>
2024-03-30 20:39:23 -0700

    bug: 2 children when I only expect 1

--- main.gd
+++ main.gd
@@ -1,13 +1,28 @@
 extends Node
 
-
+var dictionary = []
+var text = preload("res://text.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var text = preload("res://text.tscn").instantiate()
-#	text.set_position(Vector2(randi_range(100, 1000), randi_range(100, 500)))
-	add_child(text)
+	load_dictionary()
+	spawn_word()
+# TODO: By the end here, we seem to have 2 words. One that was properly spawned. The other has null text
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+func load_dictionary():
+	var file = FileAccess.open('res://dictionary.txt', FileAccess.READ)
+	while not file.eof_reached():
+		var line = file.get_line()
+		if (!line.is_empty()):
+			dictionary.push_back(String(line))
+	file.close()
+
+func spawn_word():
+	var text_instance = text.instantiate()
+	text_instance.set_position(Vector2(randi_range(100, 1000), randi_range(100, 500)))
+	text_instance.set_word(dictionary.pick_random())
+	add_child(text_instance)
--- text.gd
+++ text.gd
@@ -5,13 +5,6 @@ signal break_streak
 
 var dictionary = []
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	load_dictionary()
-	text = dictionary.pick_random()
-	print(text)
-	print(text.length())
-
 func _input(event):
 	if event is InputEventKey and event.is_pressed():
 		contains_and_emit(event.as_text_key_label().to_lower())
@@ -20,20 +13,18 @@ func _input(event):
 func _process(_delta):
 	pass
 
-func load_dictionary():
-	var file = FileAccess.open('res://dictionary.txt', FileAccess.READ)
-	while not file.eof_reached():
-		var line = file.get_line()
-		if (!line.is_empty()):
-			dictionary.push_back(String(line))
-	file.close()
-
 func contains_and_emit(key_label):
+	print(text)
 	if text.substr(0, 1).contains(key_label):
 		update_text()
 		update_score.emit()
 	else:
+		print("Within the else")
+		print(text)
 		break_streak.emit()
+
+func set_word(word):
+	text = word
 
 func update_text():
 	text = text.substr(1, text.length() + 1)

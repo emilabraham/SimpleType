61249759e6d4b2c6767fb4358c90078d6c660e96
Emil Abraham <emilabraham1414@gmail.com>
2024-08-08 07:12:23 -0700

    WIP: Reworking collision

--- Bullet.gd
+++ Bullet.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends CharacterBody2D
 
 signal destroy_enemy
 
@@ -16,8 +16,16 @@ func _fire(word):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # Moves toward the word, then deletes itself
 func _process(delta):
+	## TODO: I want the bullet to travel towards the target_enemy, then report when it collides
 	if (target_enemy):
-		position = position.move_toward(target_enemy.position, delta * speed)
+		#position = position.move_toward(target_enemy.position, speed)
+		var collision = move_and_collide(position.move_toward(target_enemy.position, speed) * delta)
+		
+		#position = position.move_toward(target_enemy.position, delta * speed)
+		if (collision != null):
+			print(collision)
+			print(collision.get_collider().name)
+			
 		if (position == target_enemy.position):
 			target_enemy.remove_word()
 			target_enemy = null

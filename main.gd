114a78230fdd62ab9cea845325c8335d3e755199
Emil Abraham <emilabraham1414@gmail.com>
2024-08-30 22:42:53 -0700

    level file

--- main.gd
+++ main.gd
@@ -2,25 +2,61 @@ extends Node
 
 var enemy1 = preload("res://Enemy1.tscn")
 var bullet = preload("res://Bullet.tscn")
+var game_data = {}
 var time_start = 0
 var time_now = 0
 var time_index = 0
 var map_index = -1
-var map = ['1','1','1','1','1','1']
+var level_index = 0
+var waiting_for_screen_clear = false
 
 const ENEMY_1 = '1'
+const END_LEVEL = 'E'
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	load_game_data()
+	load_next_level()
 	time_start = Time.get_unix_time_from_system()
 	$HUD.emit_highest_score.connect($GameOver._on_update_highest_score)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	calculate_elapsed_time()
+	if (!waiting_for_screen_clear):
+		pass_time()
+	else:
+		check_to_load_next_level()
+
+func load_game_data():
+	var filename = "res://game_data.txt"
+	var file = FileAccess.open(filename, FileAccess.READ)
+	for i in file.get_as_text().count(":"):
+		var line = file.get_line()
+		if line.contains(":"):
+			var value = line.split(":")[1]
+			value = value.trim_prefix("[")
+			value = value.trim_suffix("],")
+			value = value.replace("\"", "")
+			value = value.split(",")
+			game_data[level_index] = value
+			level_index = level_index + 1
+	file.close()
+	level_index = -1
+
+func check_to_load_next_level():
+	if get_tree().get_nodes_in_group("enemies").size() == 0:
+		load_next_level()
+		waiting_for_screen_clear = false
+
+func load_next_level():
+	level_index = level_index + 1
+	map = game_data[level_index]
+	time_start = Time.get_unix_time_from_system()
+	time_index = 0
+	map_index = -1
 
 # Keep the in-game time updated
-func calculate_elapsed_time():
+func pass_time():
 	time_now = Time.get_unix_time_from_system()
 	var time_elapsed = round(time_now - time_start)
 	if (time_elapsed > time_index):
@@ -35,8 +71,13 @@ func increment_map_index():
 		choose_spawn(current_spawn)
 
 func choose_spawn(current_spawn):
-	if (current_spawn == ENEMY_1):
-		spawn_enemy1()
+	match current_spawn:
+		ENEMY_1:
+			spawn_enemy1()
+		END_LEVEL:
+			waiting_for_screen_clear = true
+		_:
+			pass
 
 func spawn_enemy1():
 	var enemy1_instance = enemy1.instantiate()

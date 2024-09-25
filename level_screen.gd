e76874e66304fdef12463adc87279e6c6495bb2f
Emil Abraham <emilabraham1414@gmail.com>
2024-09-24 20:34:10 -0700

    fix level screen

--- level_screen.gd
+++ level_screen.gd
@@ -12,5 +12,7 @@ func _process(_delta: float) -> void:
 func _on_update_level(level):
 	$LevelNumberLabel.set_level(level)
 	visible = true
+	get_tree().paused = true
 	await get_tree().create_timer(3).timeout
+	get_tree().paused = false
 	visible = false

74f30358fd6bc4a3a328484798eb6a42afb42f10
Emil Abraham <emilabraham1414@gmail.com>
2024-09-25 07:01:02 -0700

    potential bullet passthrough

--- Bullet.gd
+++ Bullet.gd
@@ -17,11 +17,21 @@ func _process(delta):
 	if (target_enemy):
 		var bullet_vector = (target_enemy.position - starting_position) * speed
 		var collision = move_and_collide(bullet_vector * delta)
-		
-		if (collision != null && collision.get_collider() == target_enemy):
+		remove_enemy(collision, target_enemy)
+		#if (collision != null && collision.get_collider() == target_enemy):
+			#target_enemy.remove_word()
+			#target_enemy = null
+			#delete_bullet()
+
+func remove_enemy(collision, target_enemy):
+	if collision != null:
+		if collision.get_collider() == target_enemy:
 			target_enemy.remove_word()
 			target_enemy = null
 			delete_bullet()
+		#If we want to allow bullet passthrough. Maybe as a powerup.
+		#else:
+			#collision.get_collider().remove_word()
 
 func delete_bullet():
 	queue_free()
